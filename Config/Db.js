1e4d6bbefc8995492ae9a8263486e6edc43234f1
brendanxure <brendanxure@outlook.com>
2023-06-13 08:24:13 +0200

    update

--- Config/Db.js
+++ Config/Db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const DBConnect = async () => {
     try {
-        await mongoose.connect("mongodb+srv://brendanxure:xurecoder2022@cluster0.vzihnbw.mongodb.net/Commerce")
+        await mongoose.connect(process.env.MONGO_URL)
         console.log("DB Connected Successfully")
     } catch (error) {
         console.log(error)
