d85a4e71e5d6a75e393211c58b1ee704895ec752
brendanxure <brendanxure@outlook.com>
2023-07-29 14:00:27 +0100

    update

--- Service/ProductService.js
+++ Service/ProductService.js
@@ -4,9 +4,24 @@ const Product = require("../Model/Product")
 
 const createProduct = async(title, desc, img, categories, size, color, price)=> {
     try {
-        const Product = await Product.create({title, desc, img, categories, size, color, price})
-        return {code: responsecodes.SUCCESS, success: true, message: 'Product Successfully Created', data: Product}
+        const product = await Product.create({title, desc, img, categories, size, color, price})
+        return {code: responsecodes.SUCCESS, success: true, message: 'Product Successfully Created', data: product}
     } catch (error) {
         return error
     }
+}
+
+const findAllProduct = async()=> {
+    try {
+        const products = await Product.find()
+        return {code: responsecodes.SUCCESS, success: true, message: 'Product Successfully Created', data: products}
+    } catch (error) {
+        return error   
+    }
+}
+
+
+module.exports = {
+    createProduct,
+    findAllProduct
 }
