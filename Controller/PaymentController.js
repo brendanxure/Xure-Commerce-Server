6f7e86c94abcc1410d0fa9b294d91964f07c8777
brendanxure <brendanxure@outlook.com>
2023-10-26 19:40:30 +0100

    update

--- Controller/PaymentController.js
+++ Controller/PaymentController.js
@@ -2,14 +2,14 @@ const { responsecodes } = require('../Constant/ResponseCode')
 const stripe = require("stripe")(process.env.STRIPE_KEY)
 
 const InitializeStripe = async(req, res)=> {
-    console.log('ok')
     const session = await stripe.checkout.sessions.create({
         line_items: [
           {
             price_data: {
               currency: 'usd',
               product_data: {
-                name: 'T-shirt',
+                name: 'Xure Commerce',
+                description: 'You are making a payment of $20',
               },
               unit_amount: 2000,
             },
@@ -19,10 +19,16 @@ const InitializeStripe = async(req, res)=> {
         mode: 'payment',
         success_url: `${process.env.CLIENT_URL}/success`,
         cancel_url: `${process.env.CLIENT_URL}/cancel`,
-      });
+      }, (stripeErr, stripeRes)=> {
+            if(stripeErr){
+                res.status(responsecodes.INTERNAL_SERVER_ERROR).json(stripeErr)
+            } else {
+                console.log(stripeRes)
+                res.status(responsecodes.SUCCESS).json(stripeRes.url)
+            }
+        });
     
-      res.redirect(session.url);
-      console.log(session.url)
+      // return res.json(session.url);
     // stripe.charges.create({
     //     source:req.body.tokenId,
     //     amount:req.body.amount,
